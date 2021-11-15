df7cc57d51028e82167989ba1d6b2eee9607ab17
Hamish Weir <hdweir@outlook.com>
2021-11-15 16:56:15 +0000

    Start Keycloak integration

---
+++ explorer-auth/src/main/java/com/curioustrout/explorer/server/auth/EmbeddedKeycloakApplication.java
@@ -0,0 +1,8 @@
+package com.curioustrout.explorer.server.auth;
+
+import org.keycloak.services.resources.KeycloakApplication;
+
+public class EmbeddedKeycloakApplication extends KeycloakApplication {
+
+    
+}
--- explorer-auth/src/main/java/com/curioustrout/explorer/server/auth/security/OAuth2AuthorizationServerConfig.java
+++
@@ -1,1 +0,0 @@
-package com.curioustrout.explorer.server.auth.security;

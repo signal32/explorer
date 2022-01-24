d335aa23388e95f2709177d39166fe8e8829a6e2
signal32 <55558643+signal32@users.noreply.github.com>
2022-01-24 11:14:03 +0000

    More descriptive view/tab names

--- explorer-app/src/views/HomePage.vue
+++ explorer-app/src/views/HomePage.vue
@@ -23,17 +23,17 @@
       <ion-tabs>
         <ion-router-outlet></ion-router-outlet>
         <ion-tab-bar slot="bottom">
-          <ion-tab-button tab="tab1" href="/tabs/tab1">
+          <ion-tab-button tab="tab1" href="/view/map">
             <ion-icon :icon="map" />
             <ion-label>Map</ion-label>
           </ion-tab-button>
 
-          <ion-tab-button tab="tab2" href="/tabs/tab2">
+          <ion-tab-button tab="tab2" href="/view/discover">
             <ion-icon :icon="compass" />
             <ion-label>Discover</ion-label>
           </ion-tab-button>
 
-          <ion-tab-button tab="tab3" href="/tabs/tab3">
+          <ion-tab-button tab="tab3" href="/view/profile">
             <ion-icon :icon="person" />
             <ion-label>Profile</ion-label>
           </ion-tab-button>

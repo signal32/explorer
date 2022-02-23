ffb8c07eca78440be794a586530f9d711d7ba654
signal32 <55558643+signal32@users.noreply.github.com>
2022-02-23 21:32:33 +0000

    Experimental floating UI above map

--- explorer-app/src/views/tabs/MapTab.vue
+++ explorer-app/src/views/tabs/MapTab.vue
@@ -1,18 +1,33 @@
 <template>
   <ion-page>
+      <div class="explorer-above">
+
+          <ion-row>
+              <ion-col size="2">
+                  <ion-button shape="round" color="light" @click="router.push('/auth')">
+                      <ion-icon slot="icon-only" :icon="planet"></ion-icon>
+                  </ion-button>
+              </ion-col>
+              <ion-col size="8">
+                  <div style="background-color: transparent; border-radius: 15pc; contain: content; box-shadow: 1px 1px 4px 2px rgba(2,2,2,0.45);">
+                      <ion-searchbar style="padding: 0"></ion-searchbar>
+                  </div>
+              </ion-col>
+              <ion-col size="2">
+                  <ion-button shape="round" color="light" @click="router.push('/debug')">
+                      <ion-icon slot="icon-only" :icon="settingsOutline"></ion-icon>
+                  </ion-button>
+              </ion-col>
+          </ion-row>
+      </div>
     <ion-content :fullscreen="true">
 
-        <ion-fab vertical="top" horizontal="start">
-            <ion-searchbar></ion-searchbar>
-        </ion-fab>
-
-
-        <ion-fab vertical="top" horizontal="end" slot="fixed">
-            <ion-fab-button color="light" @click="move()">
+        <ion-fab vertical="top" horizontal="end" slot="fixed" style="margin-top: 60px">
+            <ion-fab-button color="light" @click="move()" >
                 <ion-icon :icon="locateOutline"></ion-icon>
             </ion-fab-button>
             <ion-fab-button color="light" @click="optionsModalOpen = true">
-                <ion-icon :icon="locateOutline"></ion-icon>
+                <ion-icon :icon="layersOutline"></ion-icon>
             </ion-fab-button>
         </ion-fab>
         <MapView v-model:position="position" @update:position="position = $event" @update:selected="setModalOpen(true, $event)">
@@ -47,24 +62,25 @@
 <script lang="ts">
 import 'maplibre-gl/dist/maplibre-gl.css'
 import {
-    IonButtons,
+    IonButtons, IonCol,
     IonContent,
     IonFab,
     IonFabButton,
     IonHeader,
     IonIcon,
     IonModal,
-    IonPage,
+    IonPage, IonRow,
     IonSearchbar,
     IonTitle,
     IonToolbar
 } from '@ionic/vue';
-import {locateOutline} from 'ionicons/icons';
+import {locateOutline, planet, settingsOutline, layersOutline} from 'ionicons/icons';
 import {defineComponent, ref, watch} from 'vue';
 import router from '@/router'
 import MapView, {MapPosition} from '@/components/MapView.vue';
 import {GeoEntity} from '@/modules/geo/entity';
 import MapOptions from '@/components/MapOptions.vue';
+import {useRouter} from 'vue-router';
 
 const defaultAbstract: GeoEntity = {
     id: 'unidentified',
@@ -74,7 +90,7 @@ const defaultAbstract: GeoEntity = {
 
 export default defineComponent({
     components: { MapOptions, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab,
-        IonFabButton, IonIcon, IonModal, IonButtons, MapView, IonSearchbar
+        IonFabButton, IonIcon, IonModal, IonButtons, MapView, IonSearchbar, IonCol, IonRow,
     },
 
     setup() {
@@ -106,8 +122,19 @@ export default defineComponent({
 
         const optionsModalOpen = ref(true);
 
-        return { position, move, locateOutline, isModalOpenRef, optionsModalOpen, setModalOpen, selectedEntityAbstractRef };
+        return { position, move, locateOutline, planet, settingsOutline, layersOutline, isModalOpenRef, optionsModalOpen, setModalOpen, selectedEntityAbstractRef, router };
     },
 
 });
 </script>
+
+<style>
+div.explorer-above {
+    position:fixed;
+/*    background:#fff4c8;
+    border:1px solid #ffcc00;*/
+    width:100%;
+    z-index:100;
+    top:5px;
+}
+</style>
